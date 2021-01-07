ae3a7a2d9bdd57e32d011a0d783fa3c2ba9030ab
Crew Spence <csspence123@gmail.com>
2021-01-07 08:26:37 -0600

    Wrote working solution.

--- codewars_NBA_full_48_minutes_average.js
+++ codewars_NBA_full_48_minutes_average.js
@@ -14,6 +14,9 @@ All inputs will be either be an integer or float.
 Follow your dreams!
 */
 
-function pointsPer48(ppg, mpg) {
-
+const pointsPer48 = (ppg, mpg) => {
+  if(ppg === 0 || mpg === 0) {
+    return 0;
+  }
+  return Number(((ppg * 48) / mpg).toFixed(1));
 }
